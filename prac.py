251f74134c72297eaf0167c5c9310013bcc8a282
AmartyaDey24 <47809229+AmartyaDey24@users.noreply.github.com>
2019-03-06 12:22:54 +0530

    Add files via upload

---
+++ prac.py
@@ -0,0 +1,18 @@
+from mod import add
+
+d = add(3,4)
+print("The addition of 3,4 is: ",d)
+
+from mod import sub
+
+d = sub(9,7)
+print("The subtraction of 9,7 is: ",d)
+
+from mod import mul
+d = mul(5,2)
+print("The multiplication of 5,2 is: ",d)
+
+
+from mod import div
+d = div(5,10)
+print("The division of 5,10 is: ",d)
