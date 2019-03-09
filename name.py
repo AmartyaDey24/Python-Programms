1a1aeb6358c5179bcabdada82bf0fbf0aef518f5
AmartyaDey24 <47809229+AmartyaDey24@users.noreply.github.com>
2019-03-09 11:57:46 +0530

    Add files via upload

---
+++ name.py
@@ -0,0 +1,8 @@
+i = str(input("Enter your name: "))
+j = str(input("Enter whice branch u r: "))
+l = str(input("Where do u live: "))
+
+def func(i,j,l):
+	print("Hii I am\t",i,"\n\tI am styding in",j,"\nI live in",l)
+	
+func(i,j,l)
